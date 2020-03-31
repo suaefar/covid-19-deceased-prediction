9915cc95882f0073de285bec318b966f8047a974
Marc René Schädler <suaefar@googlemail.com>
2020-04-01 01:30:20 +0200

    Use timeseries data instead of daily reports to include US
    Remove many countries with low numbers to clean up the figure.

--- plot_data.m
+++ plot_data.m
@@ -3,34 +3,36 @@ close all;
 clear;
 clc;
 
-days = 22:30;
-days_predicion = 22:33;
-days_last = days(end);
+days = 12:30;
+past_days_fit = 11;
+future_days_predicion = 3;
 
-regions = {'Italy' 'Spain' 'France' 'UK' 'Netherlands' 'Germany' 'Belgium' 'Switzerland' 'Portugal' 'Austria'};
+regions = {'Italy' 'Spain' 'France' 'US' 'UK' 'Netherlands' 'Germany'};
 data = importdata('data.txt');
 num_regions = size(data,1);
 
 colors = lines(num_regions);
 
 graphics_toolkit qt;
-figure('Position',[0 0 400 500]);
+figure('Position',[0 0 500 500]);
 h = [];
 
 for i=1:num_regions
   deceased = data(i,:);
-  valid_days = ~isnan(deceased);
-  h(end+1) = plot(days(valid_days),log2(deceased(valid_days)),'o','color',colors(i,:));
+  h(end+1) = plot(days,log2(deceased),'o','color',colors(i,:));
   hold on;
-  P = fminsearch(@(P) sum((log2(deceased(valid_days)) - (P(1) + P(2).*(days(valid_days)-days_last))).^2),[0;0]);
-  prediction_linear = P(1) + P(2).*(days_predicion-days_last);
-  plot(days_predicion,prediction_linear,'--','color',colors(i,:));
-  P = fminsearch(@(P) sum((log2(deceased(valid_days)) - (P(1) + P(2).*(days(valid_days)-days_last) + P(3).*(days(valid_days)-days_last).^2)).^2),[P(1);P(2);0]);
-  prediction_quadratic = P(1) + P(2).*(days_predicion-days_last) + P(3).*(days_predicion-days_last).^2;
-  plot(days_predicion,prediction_quadratic,'-','color',colors(i,:));
+  y = log2(deceased(end-past_days_fit+1:end));
+  x = -past_days_fit+1:0;
+  P = fminsearch(@(P) sum((y - (P(1) + P(2).*x)).^2),[0;0]);
+  xi = 1-past_days_fit:future_days_predicion;
+  prediction_linear = P(1) + P(2).*xi;
+  plot(days(end)+xi,prediction_linear,'--','color',colors(i,:));
+  P = fminsearch(@(P) sum((y - (P(1) + P(2).*x + P(3).*x.^2)).^2),[P(1);P(2);0]);
+  prediction_quadratic = P(1) + P(2).*xi + P(3).*xi.^2;
+  plot(days(end)+xi,prediction_quadratic,'-','color',colors(i,:));
   next_day_prediction = P(1) + P(2) + P(3);
   today_prediction = P(1);
-  plot(days_last+1,next_day_prediction,'x','color',colors(i,:));
+  plot(days(end)+1,next_day_prediction,'x','color',colors(i,:));
   regions{i} = [regions{i} ' x - ' num2str(2.^(next_day_prediction),'%5.0f') ' (' num2str(2.^(next_day_prediction)-2.^(today_prediction),'%+5.0f')  ')'];
 end
 ylabel('Deceased');
@@ -38,13 +40,12 @@ xlabel('Days in March');
 ytickvals = 2.^(0:0.5:20);
 yticks(log2(ytickvals));
 yticklabels(round(ytickvals));
-xticks(days_predicion);
-xlim(days_predicion([1,end]));
+xticks(days(1):days(end)+future_days_predicion);
 grid on;
-axis image;
-ylim([0 log2(20000)]);
+xlim([days(1)-1 days(end)+future_days_predicion+1]);
+ylim([2 15]);
 legend(h,regions,'location','southeast');
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 5].*1.4);
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 5 5].*1.4);
 drawnow;
 print('-depsc2','prediction.eps');
 print('-dpng','prediction.png');
--- update_data.sh
+++ update_data.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-REGIONS=(',Italy,' ',Spain,' 'France,France,' 'United Kingdom,United Kingdom' 'Netherlands,Netherlands,' ',Germany,' ',Belgium,' ',Switzerland,' ',Portugal,' ',Austria,')
-DATES=( 03-22-2020 \
-        03-23-2020 \
-        03-24-2020 \
-        03-25-2020 \
-        03-26-2020 \
-        03-27-2020 \
-        03-28-2020 \
-        03-29-2020 \
-        03-30-2020 \
-      )
+REGIONS=(',Italy,' ',Spain,' ',France,' ',US,' ',United Kingdom' ',Netherlands,' ',Germany,')
+FIELDS="55-73"
 
 for REGION in "${REGIONS[@]}"; do
-  for DATE in "${DATES[@]}"; do
-    MAGICTOKEN=$(head -n1 "COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/${DATE}.csv" | sed 's/^[^A-Za-z0-9]*//' | cut -d, -f1)
-    if [ "${MAGICTOKEN}" == "FIPS" ]; then
-      REGION_TMP=$(echo ${REGION} | sed 's/[^,]*//')
-      VALUE=$(grep ",${REGION_TMP// /\\ }" "COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/${DATE}.csv" | cut -d, -f9 | tr -d "\n")
-    else
-      VALUE=$(grep "${REGION// /\\ }" "COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/${DATE}.csv" | cut -d, -f5 | tr -d "\n")
-    fi
-    if [ -n "$VALUE" ]; then
-      echo -n "$VALUE "
-    else
-      echo -n "Nan "
-    fi
-  done
-  echo ""
+  grep "^${REGION}" COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv | cut -d, -f${FIELDS} | tr "," " "
 done > data.txt
 
 ./plot_data.m
