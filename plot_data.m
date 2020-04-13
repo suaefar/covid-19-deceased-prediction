b5fd597775164bb05fffb99c98a6be4fdc6a13de
Marc René Schädler <suaefar@googlemail.com>
2020-04-13 13:47:17 +0200

    Add classification of "course"

--- plot_data.m
+++ plot_data.m
@@ -16,6 +16,7 @@ colors = lines(num_regions);
 graphics_toolkit qt;
 figure('Position',[0 0 500 500]);
 h = [];
+Ps = cell(1,num_regions);
 
 for i=1:num_regions
   deceased = data(i,:);
@@ -31,6 +32,7 @@ for i=1:num_regions
   prediction_quadratic = P(1) + P(2).*xi + P(3).*xi.^2;
   plot(days(end)+xi,prediction_quadratic,'-','color',colors(i,:));
   next_day_prediction = P(1) + P(2) + P(3);
+  Ps{i} = P;
   today_prediction = P(1);
   plot(days(end)+1,next_day_prediction,'x','color',colors(i,:));
   regions{i} = [regions{i} ' x - ' num2str(2.^(next_day_prediction),'%5.0f') ' (' num2str(2.^(next_day_prediction)-2.^(today_prediction),'%+5.0f')  ')'];
@@ -49,3 +51,28 @@ set(gcf,'PaperUnits','inches','PaperPosition',[0 0 5 5].*1.4);
 drawnow;
 print('-depsc2','prediction.eps');
 print('-dpng','prediction.png');
+
+
+figure('Position',[0 0 500 500]);
+h = [];
+plot([-0.3 0.3],[0 0],'-k');
+hold on;
+plot([0 0],[-0.01 0.01],'-k');
+text(-0.15,0.005,'Slope negative and increasing','horizontalalignment','center')
+text(0.15,0.005,'Slope positve and increasing','horizontalalignment','center')
+text(0.15,-0.005,'Slope positve and decreasing','horizontalalignment','center')
+for i=1:num_regions
+  P = Ps{i};
+  h(end+1) = scatter(P(2), P(3), sqrt(2.^P(1)), colors(i,:),'filled');
+end
+ylabel('Curvature');
+xlabel('Slope');
+xticks([-0.3:0.05:-0.05 0 0.05:0.05:0.3]);
+yticks([-0.01:0.001:-0.001 0 0.001:0.001:0.01]);
+yticks();
+grid on;
+legend(h,regions,'location','southwest');
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 5 5].*1.4);
+drawnow;
+print('-depsc2','classification.eps');
+print('-dpng','classification.png');
