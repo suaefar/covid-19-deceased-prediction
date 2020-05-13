633097ee8ea5ea3b1d8cfa1237bd6e2f1470de8f
Marc René Schädler <suaefar@googlemail.com>
2020-05-13 09:05:33 +0200

    Update data

--- plot_data.m
+++ plot_data.m
@@ -3,7 +3,7 @@ close all;
 clear;
 clc;
 
-days = -12:10;
+days = -10:12;
 past_days_fit = 14;
 future_days_predicion = 3;
 
--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REGIONS=(',US,' ',United Kingdom,' ',Spain,' ',Brazil,' ',Germany,' ',Mexico,' ',India,' ',Russia,' ',Sweden,')
-FIELDS="92-114"
+FIELDS="94-116"
 
 for REGION in "${REGIONS[@]}"; do
   grep "^${REGION}" COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv | cut -d, -f${FIELDS} | tr "," " "
