32c736dcdd9b58c3f656b1be8f43753668b37efd
Marc René Schädler <suaefar@googlemail.com>
2020-06-02 08:18:24 +0200

    Update data

--- plot_data.m
+++ plot_data.m
@@ -3,7 +3,7 @@ close all;
 clear;
 clc;
 
-days = -22:0 + 0;
+days = (-22:0) + 1;
 past_days_fit = 14;
 future_days_predicion = 3;
 
@@ -42,7 +42,7 @@ for i=1:num_regions
   regions_info{i} = [regions{i} ' x - ' num2str(2.^(next_day_prediction),'%5.0f') ' (' num2str(2.^(next_day_prediction)-2.^(today_prediction),'%+5.0f')  ')'];
 end
 ylabel('Deceased');
-xlabel('Days in May');
+xlabel('Days in June');
 ytickvals = 2.^(0:0.5:21);
 yticks(log2(ytickvals));
 yticklabels(round(ytickvals));
--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REGIONS=(',US,' ',United Kingdom,' ',Spain,' ',Brazil,' ',Germany,' ',Mexico,' ',India,' ',Russia,' ',Sweden,')
-FIELDS="113-135"
+FIELDS="114-136"
 
 for REGION in "${REGIONS[@]}"; do
   grep "^${REGION}" COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv | cut -d, -f${FIELDS} | tr "," " "
