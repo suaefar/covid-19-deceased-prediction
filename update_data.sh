e62ea3300865a5ee19759087e3287e8d6fa38adf
Marc René Schädler <suaefar@googlemail.com>
2020-06-12 09:52:37 +0200

    Update data

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REGIONS=(',US,' ',United Kingdom,' ',Brazil,' ',Spain,' ',Mexico,' ',Germany,' ',India,' ',Russia,' ',Sweden,')
-FIELDS="123-145"
+FIELDS="124-146"
 
 for REGION in "${REGIONS[@]}"; do
   grep "^${REGION}" COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv | cut -d, -f${FIELDS} | tr "," " "
